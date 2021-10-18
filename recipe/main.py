f36f5fc3c1870b194a04c6e8cbdf5c632fe63956
ParnThanatibordee <parn8946@gmail.com>
2021-10-18 15:11:55 +0700

    refactor main.py

--- recipe/main.py
+++ recipe/main.py
@@ -1,26 +1,16 @@
 from recipe import Recipe
 
 if __name__ == '__main__':
-    recipe1 = Recipe("Coffee with sugar")
-    recipe1.coffee = 4
-    recipe1.sugar = 2
-    recipe1.milk = 0
-    recipe1.price = 30.0
-    print(recipe1)
+    recipe_list = [["Coffee with sugar", 4, 0, 0, 2, 30.0],
+                   ["Latte", 3, 0, 1, 2, 40.0],
+                   ["Hot Chocolate", 0, 3, 4, 2, 30.0],
+                   ]
 
-    recipe2 = Recipe("Latte")
-    recipe2.coffee = 3
-    recipe2.sugar = 2
-    recipe2.milk = 1
-    recipe2.price = 40.0
-    print(recipe2)
-
-
-    recipe3 = Recipe("Hot Chocolate")
-    recipe3.coffee = 0
-    recipe3.chocolate = 3
-    recipe3.sugar = 2
-    recipe3.milk = 4
-    recipe3.price = 30.0
-    print(recipe3)
-    
+    for i in range(len(recipe_list)):
+        recipe = Recipe(recipe_list[i][0])
+        recipe.coffee = recipe_list[i][1]
+        recipe.chocolate = recipe_list[i][2]
+        recipe.milk = recipe_list[i][3]
+        recipe.sugar = recipe_list[i][4]
+        recipe.price = recipe_list[i][5]
+        print(recipe)
