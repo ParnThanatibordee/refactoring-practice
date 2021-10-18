820adb20aa6a73a786f95763dcef829c4924c546
ParnThanatibordee <parn8946@gmail.com>
2021-10-18 15:21:21 +0700

    edit main.py

--- recipe/main.py
+++ recipe/main.py
@@ -1,16 +1,23 @@
 from recipe import Recipe
 
-if __name__ == '__main__':
-    recipe_list = [["Coffee with sugar", 4, 0, 0, 2, 30.0],
-                   ["Latte", 3, 0, 1, 2, 40.0],
-                   ["Hot Chocolate", 0, 3, 4, 2, 30.0],
-                   ]
 
-    for i in range(len(recipe_list)):
-        recipe = Recipe(recipe_list[i][0])
-        recipe.coffee = recipe_list[i][1]
-        recipe.chocolate = recipe_list[i][2]
-        recipe.milk = recipe_list[i][3]
-        recipe.sugar = recipe_list[i][4]
-        recipe.price = recipe_list[i][5]
-        print(recipe)
+def create_order(name="Coffee", coffee=0, chocolate=0, milk=0, sugar=0, price=0):
+    order = Recipe(name)
+    order.coffee = coffee
+    order.chocolate = chocolate
+    order.milk = milk
+    order.sugar = sugar
+    order.price = price
+
+    return order
+
+
+if __name__ == '__main__':
+    recipe1 = create_order("Coffee with sugar", 4, 0, 0, 2, 30.0)
+    print(recipe1)
+
+    recipe2 = create_order("Latte", 3, 0, 1, 2, 40.0)
+    print(recipe2)
+
+    recipe3 = create_order("Hot Chocolate", 0, 3, 4, 2, 30.0)
+    print(recipe3)
